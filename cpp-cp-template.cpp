488e496c4087809af64e2db223a5d94761b2f982
Vaibhav <59314660+ItsMrVaibhav@users.noreply.github.com>
2022-06-20 02:14:50 +0530

    Create cpp-cp-template.cpp

---
+++ cpp-cp-template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <stack>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+
+using namespace std;
+
+#define INF (int) 1e9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef long long ll;
+
+
+ll msb(ll x) {
+	ll k = (ll)(log(n) / log(2));
+    return 1 << k;
+}
+
+ll lsb(ll x) {
+	return x & (-x);
+}
+
+int main() {
+	ll t;
+	cin >> t;
+
+	while (t--) {
+		// code here
+	}
+
+	return 0;
+}
